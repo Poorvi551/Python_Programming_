ba34b34412d3a6f753f7fc0434025a20a45da694
S B Poorvi <182614650+Poorvi551@users.noreply.github.com>
2026-06-30 10:04:18 +0530

    Update findfirstrepeating.py

--- findfirstrepeating.py
+++ findfirstrepeating.py
@@ -1,13 +1,22 @@
 # wap to find first repeating element
-def first_repeating(l):
-    res=[]
-    for i in l:
-        for j in range(i+1,len(l)):
-            if l[i]==l[j]:
-                res.append(l[i])
-    return res
-l=eval(input("Enter list:"))    #[1,2,3,2,1]
-print(first_repeating(l))
+
+l=eval(input("Enter list:"))
+res=[]
+for i in l:
+    if i in out:
+        print(i)
+        break
+    res.append(i)
+# def first_repeating(l):
+#     res=[]
+#     for i in l:
+#         for j in range(i+1,len(l)):
+#             if l[i]==l[j]:
+#                 res.append(l[i])
+#     return res
+# l=eval(input("Enter list:"))    #[1,2,3,2,1]
+# print(first_repeating(l))
+
 
 
 # or
