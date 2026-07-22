b6215e5e5ee92e0f6f8fb7487fbcb7d9121e2b08
S B Poorvi <182614650+Poorvi551@users.noreply.github.com>
2026-07-22 19:53:59 +0530

    Update checksorted.py

--- checksorted.py
+++ checksorted.py
@@ -7,3 +7,16 @@ def check_sorted(l):
     print("List is sorted")
 l=eval(input("Enter list:"))
 check_sorted(l)
+
+# or
+
+l=eval(input("Enter list:"))
+flag=True
+for i in range(len(l) - 1):
+    if l[i] > l[i + 1]:
+        flag=False
+        break
+if flag:
+    print("Sorted")
+else:
+    print("not sorted")
